8a82fc759651ad9ab46fbb88af735638ddc0fd51
= <=>
2022-02-20 17:47:23 +0600

    simple js code added

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function c() {
+    const credit = document.getElementById('credit').value;
+    const grade = document.getElementById('grade').value;
+
+    const point = credit * grade;
+
+    document.getElementById('point').value = point;
+    const cgpa = point / credit;
+   
+    return document.getElementById('showResult').innerHTML = cgpa;
+}
+
+function reset(){
+    const credit = document.getElementById('credit').value = ""
+    const grade = document.getElementById('grade').value = ""
+    const point = document.getElementById('point').value = ""
+    return {credit, grade, point};
+}
+
+c();
+reset();
